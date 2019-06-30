93de1f5afb75eaa8c8106b3fe770e18377233d1e
Adriano Neres <ah.driano@gmail.com>
2019-06-30 15:26:14 -0400

    scheduling and sending mail configurations

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '5d95fa0dc1a949f04b0b475b99490c30',
+  expiresIn: '7d',
+};
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '1f6ca4f2aecc70',
+    pass: '57bc0dafc6153a',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
